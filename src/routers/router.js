876fa98b9c2741aeb63d09a755d59cb52331f72c
Radovan Rusz <radovan.rusz@trz.cz>
2020-01-17 11:37:13 +0100

    add post

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,6 +4,16 @@ const router = new express.Router();
 require('../db/mongoose');
 const Right = require('../models/right');
 
+router.post('/coco', (req, res) => {
+    const right = new Right(req.body);
+    right.save().then(() => {
+      res.send(right);
+    }).catch((error) => {
+      res.status(500).send(error);
+    });
+  });
+
+
 router.get('/coco', (req, res) => {
     console.log(req.query);
     const params = {};
